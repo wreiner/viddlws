45ce5ae839323081021ca27c26f75b03ceedd13b
Walter Reiner <walter.reiner@wreiner.at>
2022-07-23 18:56:34 +0200

    Remove core urls conf as it is not used

--- viddlws/core/urls.py
+++ viddlws/core/urls.py
@@ -1,28 +1,4 @@
-from django.conf.urls import url
-
-from viddlws.core.views import TagListView, VideoCreate, VideoDetail, VideoListView
-
 app_name = "core"
 urlpatterns = [
-    # core
-    url(
-        r"^$",
-        VideoListView.as_view(),
-    ),
-    url(
-        r"^v/(?P<pk>[\d]+)/$",
-        VideoDetail.as_view(),
-    ),
-    url(
-        r"^add_video/$",
-        VideoCreate.as_view(),
-    ),
-    url(
-        r"^tags/$",
-        TagListView.as_view(),
-    ),
-    url(
-        r"^tags/(?P<slug>[-\w]+)/$",
-        VideoListView.as_view(),
-    ),
+    # core urls are located in site wide urls.
 ]
