8e88336b452494ce9241ebcba707a3b98a27827e
Walter Reiner <walter.reiner@wreiner.at>
2022-07-25 18:14:30 +0200

    Add more functional tests

--- functional_tests/03-test-delete-video.py
+++ functional_tests/03-test-delete-video.py
@@ -15,7 +15,7 @@ class ReturningUser(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_login_with_registered_username(self):
+    def test_can_delete_video(self):
         # User has heard about a cool new online service to archive videos and tag them.
         # User decides to visit its homepage.
         self.browser.get(self.url)
@@ -47,10 +47,50 @@ class ReturningUser(unittest.TestCase):
         self.browser.find_element("css selector", login_button).click()
         time.sleep(1)
 
+        self.browser.find_element("id", "djHideToolBarButton").click()
+        time.sleep(1)
+
         # User is greeted with Your Videos page.
         header_text = self.browser.find_element("tag name", "h1").text
         self.assertIn("Your Videos", header_text)
         time.sleep(1)
+
+        card_titles_selector = 'h5[class="card-title list-title truncate"]'
+        card_titles = self.browser.find_elements("css selector", card_titles_selector)
+        self.assertIn(
+            "test add video modified", [card_title.text for card_title in card_titles]
+        )
+
+        # User clicks on the video they want to delete.
+        self.browser.find_element("link text", "test add video modified").click()
+        time.sleep(1)
+
+        # User clicks on the delete video button.
+        delete_button = 'a[class="btn btn-danger"]'
+        self.browser.find_element("css selector", delete_button).click()
+        time.sleep(1)
+
+        # User is displayed the delete confirmation dialog.
+        header_text = self.browser.find_element("tag name", "h1").text
+        self.assertIn("Delete video test add video modified", header_text)
+        time.sleep(1)
+
+        # User confirms the deletion by pressing the delete buttion.
+        delete_button = 'button[class="btn btn-danger"]'
+        self.browser.find_element("css selector", delete_button).click()
+        time.sleep(1)
+
+        # User is shown the Your Videos page.
+        header_text = self.browser.find_element("tag name", "h1").text
+        self.assertIn("Your Videos", header_text)
+        time.sleep(1)
+
+        # The deleted video is not in the list anymore.
+        card_titles_selector = 'h5[class="card-title list-title truncate"]'
+        card_titles = self.browser.find_elements("css selector", card_titles_selector)
+        self.assertNotIn(
+            "test add video modified", [card_title.text for card_title in card_titles]
+        )
 
 
 if __name__ == "__main__":
