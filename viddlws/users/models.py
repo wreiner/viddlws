123ed335726f1e51e98771125bb361d45d543b89
Walter Reiner <walter.reiner@wreiner.at>
2022-08-08 18:25:13 +0200

    Fix createsuperuser to add email address

--- viddlws/users/models.py
+++ viddlws/users/models.py
@@ -1,7 +1,49 @@
+from allauth.account.models import EmailAddress
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, email, password, **extra_fields):
+        if not email:
+            raise ValueError("No email address given.")
+
+        email = self.normalize_email(email)
+        user = self.model(
+            username=username,
+            email=email,
+            **extra_fields,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        if user.is_superuser:
+            EmailAddress.objects.create(
+                user=user, email=email, primary=True, verified=True
+            )
+
+        return user
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self._create_user(username, email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -15,6 +57,7 @@ class User(AbstractUser):
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+    objects = UserManager()
 
     def get_absolute_url(self):
         """Get url for user's detail view.
